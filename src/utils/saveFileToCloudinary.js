a6fd7b9f8c60d4611c175d011519f6191cb1d7a4
ViktoriaPanchenko <pikv219@gmail.com>
2025-04-09 20:15:18 +0300

    hw7

--- src/utils/saveFileToCloudinary.js
+++ src/utils/saveFileToCloudinary.js
@@ -3,7 +3,6 @@ import fs from 'node:fs/promises';
 import getEnvVar from '../utils/getEnvVar.js';
 import { CLOUDINARY } from '../constants/index.js';
 
-
 cloudinary.v2.config({
   secure: true,
   cloud_name: getEnvVar(CLOUDINARY.CLOUD_NAME),
@@ -13,14 +12,11 @@ cloudinary.v2.config({
 
 export const saveFileToCloudinary = async (file) => {
   try {
-   
     await fs.access(file.path);
-    
+
     const response = await cloudinary.v2.uploader.upload(file.path);
 
- 
     await fs.unlink(file.path);
-    
 
     return response.secure_url;
   } catch (err) {
@@ -28,3 +24,22 @@ export const saveFileToCloudinary = async (file) => {
     throw err;
   }
 };
+
+// import cloudinary from 'cloudinary';
+// import fs from 'node:fs/promises';
+
+// import getEnvVar from '../utils/getEnvVar.js';
+// import { CLOUDINARY } from '../constants/index.js';
+
+// cloudinary.v2.config({
+//   secure: true,
+//   cloud_name: getEnvVar(CLOUDINARY.CLOUD_NAME),
+//   api_key: getEnvVar(CLOUDINARY.API_KEY),
+//   api_secret: getEnvVar(CLOUDINARY.API_SECRET),
+// });
+
+// export const saveFileToCloudinary = async (file) => {
+//   const response = await cloudinary.v2.uploader.upload(file.path);
+//   await fs.unlink(file.path);
+//   return response.secure_url;
+// };
